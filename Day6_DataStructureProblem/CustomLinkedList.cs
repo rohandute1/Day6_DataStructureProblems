9494e321c01b69f5dbc8256ae630b4c41c1d6b0e
Rohan <rohandute1@gmail.com>
2023-10-03 21:16:13 +0530

    [rohan] Added Custom LinkedList implementation successfully

---
+++ Day6_DataStructureProblem/CustomLinkedList.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_DataStructureProblem
+{
+    public class CustomLinkedList<T>
+    {
+        private class Node
+        {
+            public T Data { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T data)
+            {
+                Data = data;
+                Next = null;
+            }
+        }
+
+        private Node head;
+        private int count;
+
+        public CustomLinkedList()
+        {
+            head = null;
+            count = 0;
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public void AddFirst(T data)
+        {
+            Node newNode = new Node(data);
+            newNode.Next = head;
+            head = newNode;
+            count++;
+        }
+
+        public void AddLast(T data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+            count++;
+        }
+
+        public bool Remove(T data)
+        {
+            if (head == null)
+                return false;
+
+            if (head.Data.Equals(data))
+            {
+                head = head.Next;
+                count--;
+                return true;
+            }
+
+            Node current = head;
+            Node previous = null;
+
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    previous.Next = current.Next;
+                    count--;
+                    return true;
+                }
+                previous = current;
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public bool Contains(T data)
+        {
+            Node current = head;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public void Print()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Day6_DataStructureProblem/Program.cs
+++ Day6_DataStructureProblem/Program.cs
@@ -11,7 +11,42 @@ namespace Day6_DataStructureProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Data Structure Problems.");
+            bool continueExecution = true;
+            while (continueExecution)
+            {
+                Console.WriteLine("Please choose program to perform:");
+                Console.WriteLine("1.Generic class custom Linkedlist datastructure");
+                int select = Convert.ToInt32(Console.ReadLine());
 
+                switch (select)
+                {
+                    case 1:
+                        CustomLinkedList<int> linkedList = new CustomLinkedList<int>();
+
+                        CustomLinkedList<int> customLinkedList = new CustomLinkedList<int>();
+
+                        customLinkedList.AddFirst(1);
+                        customLinkedList.AddLast(2);
+                        customLinkedList.AddLast(3);
+
+                        Console.WriteLine("Custom Linked List:");
+                        customLinkedList.Print(); 
+
+                        customLinkedList.Remove(2);
+
+                        Console.WriteLine("Custom Linked List after removing 2:");
+                        customLinkedList.Print(); 
+
+                        Console.WriteLine("Does the custom linked list contain 2? " + customLinkedList.Contains(2)); 
+                        break;
+                }
+                Console.WriteLine("Do you want to continue.(yes/no)");
+                string userInput = Console.ReadLine();
+                if (userInput != "yes")
+                {
+                    continueExecution = false;
+                }
+            }
             Console.ReadLine();
         }
     }
